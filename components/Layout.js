bbe57f31c4f0ee39dd8b77788ba3b1fa7d3579a0
fukunaga-fv <fukunaga.s@fvs-net.co.jp>
2022-07-14 10:35:57 +0900

    [add]search_resutaurants
    [fix]Restaurants/index_bug

--- components/Layout.js
+++ components/Layout.js
@@ -1,49 +1,49 @@
 import React from "react";
-import App, { Container } from "next/app";//継承用
-import Head from "next/head";//メタ情報コンポ
+import App, { Container } from "next/app"; //継承用
+import Head from "next/head"; //メタ情報コンポ
 import { Nav, NavItem } from "reactstrap";
 import Link from "next/link";
 
 const Layout = (props) => {
-	return (
-		<div>
-			<Head>
-				<title>フードデリバリーサービス</title>
-				<link 
-						rel="stylesheet" 
-						href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
-					/>
-			</Head>
-			<header>
-				<style jsx>
-					{`
-						a {
-							color: white;
-						}
-					`}
-				</style>
-				<Nav className="navbar navbar-dark bg-dark">
-					<NavItem>
-						<Link href="/">
-							<a className="navbar-brand">Home</a>
-						</Link>
-					</NavItem>
-					<NavItem className="ml-auto">
-						<Link href="/login">
-							<a className="nav-link">サインイン</a>
-						</Link>
-					</NavItem>
-					<NavItem className="ml-auto">
-						<Link href="/register">
-							<a className="nav-link">サインアップ</a>
-						</Link>
-					</NavItem>
-					
-				</Nav>
-			</header>
-			<Container>{props.children}</Container>{/* appで全てのページに渡される */}
-		</div>
-	);
-}
+  return (
+    <div>
+      <Head>
+        <title>フードデリバリーサービス</title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        />
+      </Head>
+      <header>
+        <style jsx>
+          {`
+            a {
+              color: white;
+            }
+          `}
+        </style>
+        <Nav className="navbar navbar-dark bg-dark">
+          <NavItem>
+            <Link href="/">
+              <a className="navbar-brand">Home</a>
+            </Link>
+          </NavItem>
+          <NavItem className="ml-auto">
+            <Link href="/login">
+              <a className="nav-link">サインイン</a>
+            </Link>
+          </NavItem>
+          <NavItem className="ml-auto">
+            <Link href="/register">
+              <a className="nav-link">サインアップ</a>
+            </Link>
+          </NavItem>
+        </Nav>
+      </header>
+      <Container>{props.children}</Container>
+      {/* appで全てのページに渡される */}
+    </div>
+  );
+};
 
 export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,45 @@
-import { Alert, Button, Col, Input, InputGroup, InputGroupText, Row } from "reactstrap";
+import { useState } from "react";
+import { Col, Input, InputGroup, InputGroupText, Row } from "reactstrap";
 import RestaurantsList from "../components/RestaurantsList";
 
 const index = () => {
-	return (
-		<div className="container-fluid">
-			<Row>
-				<Col>
-					<div className="search">
-					<InputGroup>
-						<InputGroupText>探す</InputGroupText>
-						<Input placeholder="レストラン名を入力してください"/>
-					</InputGroup>
-					</div>
-					<RestaurantsList/>
-				</Col>
-			</Row>
-			<style jsx>{`
+  const [query, setQuery] = useState(""); //関数コンポーネントでstateを管理
+
+  return (
+    <div className="container-fluid">
+      <Row>
+        <Col>
+          <div className="search">
+            <InputGroup>
+              <InputGroupText>探す</InputGroupText>
+              <Input
+                placeholder="レストラン名を入力してください"
+                onChange={(huga) =>
+                  setQuery(huga.target.value.toLocaleLowerCase())
+                }
+              />
+            </InputGroup>
+          </div>
+          <RestaurantsList search={query} />
+        </Col>
+      </Row>
+      <style jsx>
+        {`
 				.search {
 					margin: 20px;
 					width: 500px;
 				},
 			`}
-			</style>
-			
-		</div>
-	);
-}
+      </style>
+    </div>
+  );
+};
 
 export default index;
+
+/**
+ * onChange 入力があるとトリガーになる
+ * 右記関数の引数に値を代入する
+ * .targetでHTML要素
+ * .valueで値
+ */
