60dea35dd9e68f08dd48150d10d347e43f08b940
fukunaga-fv <fukunaga.s@fvs-net.co.jp>
2022-07-14 14:20:35 +0900

    [create]login_regster
    [fix]before_Can_not_propaty

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,46 @@
 import React from "react";
-import App from "next/app";//継承用
-import Head from "next/head";//メタ情報コンポ
+import App from "next/app"; //継承用
+import Head from "next/head"; //メタ情報コンポ
 import Layout from "../components/Layout";
 import withData from "../lib/apollo";
+import AppContext from "../context/AppContext";
 
 class MyApp extends App {
-	render() {//OR
-		const { Component, pageProps } = this.props;
-		return (
-			<>
-			{/* タイトル属性を決めたりリンクを決めたりメタ情報を含む */}
-				<Head>
-					<link 
-						rel="stylesheet" 
-						href="http://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" 
-					/>
-				</Head>
-				<Layout>
-					<Component {...pageProps} />{/* 全てのコンポを指す */}
-				</Layout>
-			</>
-		)
-	}
+  // クラスコンポはuseStateが使えないらしいk
+  // setStateはstateが更新されると再レンダーする
+  state = {
+    user: null,
+  };
+
+  setUser = (user) => {
+    this.setState({ user });
+  };
+
+  render() {
+    //OR
+    const { Component, pageProps } = this.props;
+    return (
+      //user: は条件分岐
+      <AppContext.Provider
+        value={{ user: this.state.user, setUser: this.setUser }}
+      >
+        <>
+          {/* タイトル属性を決めたりリンクを決めたりメタ情報を含む */}
+          <Head>
+            <link
+              rel="stylesheet"
+              href="http://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+            />
+          </Head>
+
+          <Layout>
+            <Component {...pageProps} />
+            {/* 全てのコンポを指す */}
+          </Layout>
+        </>
+      </AppContext.Provider>
+    );
+  }
 }
 
 export default withData(MyApp);
---
+++ pages/login.js
@@ -0,0 +1,100 @@
+import { useContext, useState } from "react";
+import {
+  Button,
+  Col,
+  Container,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+} from "reactstrap";
+import AppContext from "../context/AppContext";
+import { login, registerUser } from "../lib/auth";
+
+const Login = () => {
+  const appContext = useContext(AppContext); //_appのコンテキストを回収
+  const [data, setData] = useState({ identifier: "", password: "" });
+  //第一引数ユーザーのデータを格納、setData関数は上記userの初期値を更新する
+
+  const handleLogin = () => {
+    login(data.identifier, data.password)
+      .then((res) => {
+        appContext.setUser(res.data.user);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleChange = (e) => {
+    //e.target.name:identifireかpasswordか入力によって動的に変わる
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <div className="paper">
+            <div className="header">
+              <h2>ログイン</h2>
+            </div>
+          </div>
+          <section className="wrapper">
+            <Form>
+              <fieldset>
+                <FormGroup>
+                  <Label>メールアドレス:</Label>
+                  <Input
+                    type="email"
+                    name="identifier"
+                    style={{ height: 50, fontSize: "1.2rem" }}
+                    onChange={(e) => handleChange(e)}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label>パスワード:</Label>
+                  <Input
+                    type="password"
+                    name="password"
+                    style={{ height: 50, fontSize: "1.2rem" }}
+                    onChange={(e) => handleChange(e)}
+                  />
+                </FormGroup>
+                <span>
+                  <a href="">パスワードをお忘れですか？</a>
+                </span>
+                <Button
+                  style={{ float: "right", width: 120 }}
+                  color="primary"
+                  onClick={() => {
+                    handleLogin();
+                  }}
+                >
+                  ログイン
+                </Button>
+              </fieldset>
+            </Form>
+          </section>
+        </Col>
+      </Row>
+
+      <style jsx>
+        {`
+          .paper {
+            text-align: center;
+            margin-top: 50px;
+          }
+          .header {
+            width: 100%;
+            margin-bottom: 30px;
+          }
+          .wrapper {
+            padding: 10px 30px 20px 30px;
+          }
+        `}
+      </style>
+    </Container>
+  );
+};
+
+export default Login;
