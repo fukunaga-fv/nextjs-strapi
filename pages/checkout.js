a0bc52d6d346f764f3800f6f488a79c385097788
fukunaga-fv <fukunaga.s@fvs-net.co.jp>
2022-07-15 10:32:07 +0900

    Storip/ordersにPOSTデータを送信
    Authrization:Bearerトークンなど

---
+++ pages/checkout.js
@@ -0,0 +1,28 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { Col, Row } from "reactstrap";
+import Cart from "../components/Cart/index";
+import CheckOutForm from "../components/Checkout/CheckOutForm";
+
+const checkout = () => {
+  const stripePromise = loadStripe(
+    "pk_test_51LLNeRIPYDgU4wsGxecgHJ1AgL80woXqrr6fZvQJvL8guOqwFE7914YFIkgi89OZi5cJaQaCWVeUHDRB6RwH1O2S001B6aA61A"
+  );
+  return (
+    <Row>
+      <Col style={{ paddingRight: 0 }} sm={{ size: 3, order: 1, offset: 2 }}>
+        <h1 style={{ margin: 20, fontSize: 20, textAlign: "center" }}>
+          チェックアウト
+        </h1>
+        <Cart />
+      </Col>
+      <Col style={{ paddingLeft: 5 }} sm={{ size: 6, order: 2 }}>
+        <Elements stripe={stripePromise}>
+          <CheckOutForm />
+        </Elements>
+      </Col>
+    </Row>
+  );
+};
+
+export default checkout;
